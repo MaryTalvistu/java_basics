f22f951d6ff8dc15d577f0d7d7ccdad6088f7640
Mary Talvistu <marytalvistu@gmail.com>
2021-09-22 11:26:27 +0300

    Week 1 fix

--- src/week1/Ex21.java
+++ src/week1/Ex21.java
@@ -9,7 +9,15 @@ public class Ex21 {
         double year = Double.parseDouble(reader.nextLine());
 
         if (year % 4 == 0 ) {
-            System.out.println("The year is a leap year.");
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("The year is a leap year.");
+                } else {
+                    System.out.println("The year is not a leap year.");
+                }
+            }else {
+                System.out.println("The year is not a leap year.");
+            }
         } else {
             System.out.println("The year is not a leap year.");
         }
